751673676b3d61e31765eebbfabf388d5f3a268d
underfullll <141158264+underfullll@users.noreply.github.com>
2024-06-14 01:29:10 +0300

    19
    вот это да!

--- Todolist_in_WPF/Todolist_in_WPF/MainWindow.xaml.cs
+++ Todolist_in_WPF/Todolist_in_WPF/MainWindow.xaml.cs
@@ -20,7 +20,6 @@ namespace Todolist_in_WPF
         {
             InitializeComponent();
             EnsureDataBase();
-            //UpdateTasksFromDatabase();
         }
 
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
@@ -43,21 +42,6 @@ namespace Todolist_in_WPF
                 TaskRepository.MigrateDataBase();
             }
         }
-
-        //private void UpdateTasksFromDatabase()
-        //{
-        //    TaskRepository taskRepository = new TaskRepository();
-        //    List<ToDoListLibrary.Task> tasksFromDatabase = taskRepository.ReadAllTasks();
-
-
-        //    tasks.Clear();
-        //    foreach (var task in tasksFromDatabase)
-        //    {
-        //        tasks.Add(task);
-        //    }
-
-        //    dataGridTasks.ItemsSource = tasks;
-        //}
 
         private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
@@ -84,102 +68,6 @@ namespace Todolist_in_WPF
         {
             this.Close();
         }
-
-
-        //private void Button_Click_Add(object sender, RoutedEventArgs e)
-        //{
-        //    if (tasks == null)
-        //    {
-        //        tasks = new List<ToDoListLibrary.Task>();
-        //    }
-
-        //    int maxId = tasks.Any() ? tasks.Max(task => task.Id) : 0;
-        //    nextId = maxId + 1;
-
-        //    ToDoListLibrary.Task newTask = new ToDoListLibrary.Task
-        //    {
-        //        Id = nextId,
-        //        Title = txtFilter3.Text,
-        //        Description = txtFilter2.Text,
-        //        IsCompleted = false,
-        //    };
-
-        //    TaskRepository taskRepository = new TaskRepository();
-        //    taskRepository.Create(newTask);
-
-        //    tasks = taskRepository.ReadAllTasks();
-
-        //    dataGridTasks.ItemsSource = tasks;
-
-        //    txtFilter3.Text = "Name Tasks";
-        //    txtFilter2.Text = "Description Tasks";
-
-        //    dataGridTasks.Items.Refresh();
-        //}
-
-        //private void Button_Click_DeliteOll(object sender, RoutedEventArgs e)
-        //{
-        //    string connectionString = "Data Source = DB_ToDoList.sqlite";
-
-        //    using (var connection = new SQLiteConnection(connectionString))
-        //    {
-        //        connection.Open();
-
-        //        using (var command = new SQLiteCommand("DELETE FROM ToDoList", connection))
-        //        {
-        //            command.ExecuteNonQuery();
-        //        }
-
-        //        connection.Close();
-        //    }
-
-        //    RefreshDataGrid();
-        //}
-
-        //private void RefreshDataGrid()
-        //{
-        //    TaskRepository taskRepository = new TaskRepository();
-        //    tasks = taskRepository.ReadAllTasks();
-        //    dataGridTasks.ItemsSource = tasks;
-        //    dataGridTasks.Items.Refresh();
-        //}
-
-        //private void EditTaskButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    selectedTaskForEdit = (ToDoListLibrary.Task)dataGridTasks.SelectedItem;
-
-        //    txtFilter3.Text = selectedTaskForEdit.Title;
-        //    txtFilter2.Text = selectedTaskForEdit.Description;
-        //}
-
-        //private void DeleteTaskButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    ToDoListLibrary.Task selectedTask = (ToDoListLibrary.Task)dataGridTasks.SelectedItem;
-
-        //    tasks.Remove(selectedTask);
-
-        //    TaskRepository taskRepository = new TaskRepository();
-        //    taskRepository.Delete(selectedTask.Id);
-
-        //    dataGridTasks.ItemsSource = tasks;
-        //    dataGridTasks.Items.Refresh();
-        //}
-
-        //private void Button_Click_Done(object sender, RoutedEventArgs e)
-        //{
-        //    if (selectedTaskForEdit != null)
-        //    {
-        //        selectedTaskForEdit.Title = txtFilter3.Text;
-        //        selectedTaskForEdit.Description = txtFilter2.Text;
-
-        //        TaskRepository taskRepository = new TaskRepository();
-        //        taskRepository.Update(selectedTaskForEdit);
-
-        //        tasks = taskRepository.ReadAllTasks();
-        //        dataGridTasks.ItemsSource = tasks;
-        //        dataGridTasks.Items.Refresh();
-        //    }
-        //}
 
         private void Account_Button_Click(object sender, RoutedEventArgs e)
         {
